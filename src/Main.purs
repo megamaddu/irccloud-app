736d70001e3343a1d4a23325002a2f859b32f5e1
Michael Trotter <spicydonuts@gmail.com>
2016-02-02 10:55:51 -0700

    Point to irccloud.com & open external links in default browser

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,13 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff
-import Control.Monad.Eff.Console
-
-import Electron
-import Electron.App
-import Electron.BrowserWindow
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Electron (ELECTRON)
+import Electron.App (Path(Home), quit, onReady, getPath, getAppPath)
+import Electron.BrowserWindow (BrowserWindowOption(WebPreferences, Height, Width), WebPreference(ZoomFactor, OverlayScrollbars), onNewWindow, webContents, loadURL, onClose, newBrowserWindow)
+import Electron.Event (preventDefault)
+import Electron.Shell (openExternal)
 
 main :: forall eff. Eff (console :: CONSOLE, electron :: ELECTRON | eff) Unit
 main = do
@@ -14,13 +15,18 @@ main = do
   log appPath
   homePath <- getPath Home
   log homePath
-  onReady do
+  onReady $ do
     log "starting..."
     mainWindow <- newBrowserWindow [ Width 1200
                                    , Height 600
                                    , WebPreferences [ OverlayScrollbars true
-                                                    , ZoomFactor 2.0
+                                                    , ZoomFactor 1.0
                                                     ]
                                    ]
     mainWindow `onClose` quit
-    mainWindow `loadURL` "http://purescript.org"
+    mainWindow `loadURL` "https://www.irccloud.com/"
+
+    webStuff <- webContents mainWindow
+    webStuff `onNewWindow` \event url -> do
+      preventDefault event
+      openExternal url
